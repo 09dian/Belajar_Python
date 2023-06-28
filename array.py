268386a90621796524d6491acace63077190b89f
09dian <09dianm@gmail.com>
2023-06-28 12:03:53 +0700

    belajar Python

---
+++ array.py
@@ -0,0 +1,8 @@
+# tipe tipe array di python
+# keterangan cars[0] untuk mencetak string jika 0 di ganti menjadi 1 BMW dan 2 adalah Toyota
+# karena jika di ibartakan cars= [0,1,2] adalah implementasi dari  cars = ["volvo", "BMW", "Toyota"]
+
+cars = ["volvo", "BMW", "Toyota"]
+print( "aku suka sekali",cars[1])
+
+# cat : silakan kembangkan sendiri
---
+++ ifelse.py
@@ -0,0 +1,6 @@
+x = 5
+
+if x > 0:
+    print("Angka x adalah positif")
+else:
+    print("Angka x adalah non-positif")
